41b7a372daab7dba7d1282e85002eaeb9167ea39
Nakul Suresh <nakulsuresh98@gmail.com>
2019-03-14 01:40:50 +0530

    removed nonworking apps and unwanted files

--- FreshBazaar2.0/FreshBazaar/app/src/main/java/com/selabg11/freshbazaar/Config.java
+++
@@ -1,6 +0,0 @@
-package com.selabg11.freshbazaar;
-
-public class Config {
-    public static final String EMAIL ="freshfoodbazaar55@gmail.com";
-    public static final String PASSWORD ="freshfood55";
-}
